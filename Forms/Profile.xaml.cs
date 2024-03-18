bfbdad2f5ba6dab2214db5ad17f7c70a228d657b
Паша Филькенков <gprogg5566@gmail.com>
2024-03-18 20:05:25 +0400

    Profile

--- Forms/Profile.xaml.cs
+++ Forms/Profile.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using practice.Database;
+using practice.Models;
+
+using System.Data.Common;
+using System.Linq;
+using System.Windows;
 
 namespace practice.Forms
 {
@@ -7,9 +12,30 @@ namespace practice.Forms
     /// </summary>
     public partial class Profile : Window
     {
-        public Profile()
+        public User User { get; set; }
+        private PracticeContext db;
+
+        public Profile(User user)
         {
+            db = new PracticeContext();
+            User = user;
+            this.DataContext = this;
             InitializeComponent();
+        }
+
+        public string CountryName
+        {
+            get
+            {
+                var userCountry = db.Countries.FirstOrDefault(c => c.Id == User.CountryID);
+                return userCountry != null ? userCountry.Name : "Неизвестная страна";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PasswordChange pc = new PasswordChange(User);
+            pc.ShowDialog();
         }
     }
 }
