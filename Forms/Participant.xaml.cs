89f9c5389239c6c83dbfc1e6e465f599d40e714c
MrCatV3 <rasulnur322@gmail.com>
2024-03-18 23:21:27 +0400

    RASULKOT

--- Forms/Participant.xaml.cs
+++ Forms/Participant.xaml.cs
@@ -1,4 +1,14 @@
-﻿using System.Windows;
+﻿using Microsoft.IdentityModel.Tokens;
+using practice.Database;
+using practice.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Linq;
 
 namespace practice.Forms
 {
@@ -7,9 +17,139 @@ namespace practice.Forms
     /// </summary>
     public partial class Participant : Window
     {
+        private PracticeContext db;
+        private User user = new User();
+
         public Participant()
         {
+            db = new PracticeContext();
             InitializeComponent();
         }
+
+        private void pass_check_Click(object sender, RoutedEventArgs e)
+        {
+            if (pass_check.IsChecked == true)
+            {
+                passBoxTxt.Text = passBox.Password;
+                passBoxTxt.Visibility = Visibility.Visible;
+                passBox.Visibility = Visibility.Hidden;
+
+                passBoxTxt2.Text = passBox2.Password;
+                passBoxTxt2.Visibility = Visibility.Visible;
+                passBox2.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                passBox.Password = passBoxTxt.Text;
+                passBox.Visibility = Visibility.Visible;
+                passBoxTxt.Visibility = Visibility.Hidden;
+
+                passBox2.Password = passBoxTxt2.Text;
+                passBox2.Visibility = Visibility.Visible;
+                passBoxTxt2.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (Char.IsDigit(e.Text, 0))
+            { e.Handled = true; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            user.Surname = SurnameTxt.Text;
+            user.Name = NameTxt.Text;
+            user.Patronomic = PatronomicTxt.Text;
+            user.Phone = PhoneTxt.Text;
+            user.Email = EmailTxt.Text;
+
+            if (pass_check.IsChecked == true)
+            {
+                user.Password = passBoxTxt.Text;
+            }
+            else
+            {
+                user.Password = passBox.Password;
+            }
+
+            if (Check())
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                this.Close();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+       
+
+        private bool Check()
+        {
+            if (SurnameTxt.Text.IsNullOrEmpty() ||
+                NameTxt.Text.IsNullOrEmpty() ||
+                PatronomicTxt.Text.IsNullOrEmpty() ||
+                PhoneTxt.Text.IsNullOrEmpty() ||
+                EmailTxt.Text.IsNullOrEmpty() ||
+                passBox.Password.IsNullOrEmpty() ||
+                passBox.Password.Length < 6 &&
+                passBox.Password != @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).+$" ||
+                passBoxTxt.Text.Length < 6 &&
+                passBoxTxt.Text != @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).+$" ||
+                passBox.Password != passBox2.Password ||
+                passBoxTxt.Text != passBoxTxt2.Text ||
+                PhoneTxt.Text.Contains("_")
+                )
+            {
+                MessageBox.Show("Проверьте заполненность полей", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (db.Users.Any(x => x.Email == EmailTxt.Text))
+            {
+                MessageBox.Show("Пользователь с такой почтой уже существует", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            var email = EmailTxt.Text;
+            if (IsValidEmail(email))
+                return true;
+            else
+            {
+                MessageBox.Show("Некорректно введена почта", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+            return Regex.IsMatch(email, pattern);
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (GenderCB.SelectedIndex)
+            {
+                case 0:
+                    {
+                        user.Gender = "мужской";
+                        break;
+                    }
+                case 1:
+                    {
+                        user.Gender = "женский";
+                        break;
+                    }
+            }
+        }
+
+        
     }
 }
