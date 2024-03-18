f5655be37a5f5d1e9e694639fe194dc8a737dc98
Михаил Давыдов <davydovmihail2504@gmail.com>
2024-03-18 21:34:31 +0400

    ашч

--- Forms/Moderator.xaml.cs
+++ Forms/Moderator.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using practice.Database;
+using practice.Models;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 
 namespace practice.Forms
 {
@@ -7,9 +12,16 @@ namespace practice.Forms
     /// </summary>
     public partial class Moderator : Window
     {
+        private PracticeContext db;
+        private List<User> users = new();
+
+
         public Moderator()
         {
+            db = new PracticeContext();
             InitializeComponent();
+            users = db.Users.Where(x => x.RoleId == 4).ToList();
+            IC.ItemsSource = users;
         }
     }
 }
