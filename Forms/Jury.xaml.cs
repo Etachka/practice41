5764f9c243ad83d909833dae04af2f1af7f5be25
Паша Филькенков <gprogg5566@gmail.com>
2024-04-09 17:08:57 +0400

    Мы справились с задачей, наверное

--- Forms/Jury.xaml.cs
+++ Forms/Jury.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using practice.Database;
 using practice.Models;
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
@@ -21,20 +22,32 @@ namespace practice.Forms
         {
             db = new PracticeContext();
             InitializeComponent();
-            activities = db.Activites.ToList();
+            var activities = db.Activites.ToList();
             IC.ItemsSource = activities;
         }
+
         public Jury(User user)
         {
             db = new PracticeContext();
             InitializeComponent();
             User = user;
             activities = db.Activites.ToList();
+
+            var moderators = db.Users.ToDictionary(u => u.Id);
+
+            foreach (var activity in activities)
+            {
+                if (moderators.ContainsKey(activity.ModeratorId))
+                {
+                    activity.Moderator = moderators[activity.ModeratorId];
+                }
+            }
             IC.ItemsSource = activities;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            
             MainWindow autorization = new ();
             autorization.Show();
             this.Close();
@@ -44,6 +57,32 @@ namespace practice.Forms
         {
             Profile profile = new Profile(User);
             profile.ShowDialog();
+        }
+
+        //public string HelloModerator()
+        //{
+        //    string ModeratorName = "";
+        //    var moderator = db.Users.FirstOrDefault(u => u.Id == activity.ModeratorId);
+
+        //    return ModeratorName = (moderator != null) ? $"{moderator.Surname} {moderator.Name} {moderator.Patronomic}" : "Unknown";
+        //}
+
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            //if (TxtModN.Tag is int ModeratorID)
+            //{
+            //    // Здесь вы должны реализовать логику поиска пользователя по Id
+            //    string userName = FindUserNameById(ModeratorID);
+
+            //    // Устанавливаем имя пользователя как текст для TextBlock
+            //    TxtModN.Text = userName;
+            //}
+        }
+
+        private void TxtModN_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
         }
     }
 }
